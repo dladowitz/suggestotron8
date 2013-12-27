1b817df43498f40f40f787bd38ff62896833078c
David Ladowitz <david@ladowitz.com>
2013-12-26 21:23:29 -0800

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Suggestotron8::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'topics#index'
 
-  resources :topics
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
